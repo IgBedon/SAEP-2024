1d4a61f3822c81aec6fa475ca5fcc2b0f8de5203
Igor Bedon de Sousa <igorbedon@gmail.com>
2024-11-22 11:05:05 -0300

    Finishing project

--- to_do_list/urls.py
+++ to_do_list/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('tasks/', views.manage_tasks, name="manage_tasks"),
     path('tasks/create/', views.create_task, name="create_task"),
     path('tasks/<int:task_id>/update/', views.update_task, name="update_task"),
+    path('tasks/<int:task_id>/update/status/', views.update_task_status, name="update_task_status"),
     path('tasks/<int:task_id>/delete/', views.delete_task, name="delete_task"),
 ]
--- to_do_list/views.py
+++ to_do_list/views.py
@@ -52,7 +52,7 @@ def create_task(request):
         )
 
         messages.success(request, "Tarefa criada com sucesso!")
-        return redirect('menu')
+        return redirect('manage_tasks')
 
     context = {
         'users': users
@@ -69,19 +69,63 @@ def manage_tasks(request):
     return render(request, 'to_do_list/tasks/manage_tasks.html', context)
 
 
-def update_task(request, environment_id, task_id):
+def update_task(request, task_id):
+    task = Task.objects.filter(id=task_id).first()
+    users = User.objects.all()
 
-    return render(request, 'to_do_list/tasks/manage_tasks.html')
+    if request.method == 'POST':
+        description = request.POST.get('description')
+        department_name = request.POST.get('department_name')
+        username = request.POST.get('username')
+        status = request.POST.get('status')
+        priority = request.POST.get('priority')
+        user = User.objects.filter(username=username).first()
+
+        Task.objects.filter(id=task.id).update(
+            description=description,
+            department_name=department_name,
+            status=status,
+            priority=priority,
+            user=user
+        )
+
+        messages.success(request, 'Tarefa editada com sucesso!')
+        return redirect('manage_tasks')
+    
+    if task:
+        context = {
+            'task': task,
+            'users': users
+        }
+    else:
+        messages.error('Tarefa não encontrada!')
+        context={}
+
+    return render(request, 'to_do_list/tasks/update_task.html', context)
 
 
-def delete_task(request, environment_id, task_id):
+def update_task_status(request, task_id):
+    task = Task.objects.filter(id=task_id).first()
+    if request.method == 'POST':
+        status = request.POST.get('status')
+        Task.objects.filter(id=task.id).update(
+            status=status
+        )
+
+        messages.success(request, 'Status editado com sucesso!')
+        return redirect('manage_tasks')
+    
+
+    return redirect('menu')
+
+
+def delete_task(request, task_id):
     task = Task.objects.filter(id=task_id).first()
 
     if request.method == 'POST':
-
         task.delete()
         messages.success(request, 'Tarefa deletada com sucesso!')
-        return redirect('environment', environment_id)
+        return redirect('manage_tasks')
     
     if task:
         context = {
